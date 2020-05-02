04c8630084f89bd780cbb0e3b99537443d57524e
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-02 13:35:51 +0200

    Lesson 67 - Setting dynamic classes

--- react-complete-guid/src/App.js
+++ react-complete-guid/src/App.js
@@ -80,10 +80,20 @@ class App extends Component {
 
     }
 
+    //let classes = ['red','bold'].join(' ');
+    let classes = [];
+    if(this.state.persons.length <=2){
+      classes.push('red'); //classes = ['red']
+    }
+
+    if(this.state.persons.length <=1){
+      classes.push('bold'); //classes = ['red', 'bold']
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button style={style}
           onClick={this.togglePersonsHandler}>Switch Name
         </button>
