cb865c7ef1fc20021d31fcecc2d826dc1ec73b44
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-02 16:37:04 +0200

    Lesson 70 - Styled Components library

--- react-complete-guid/src/App.js
+++ react-complete-guid/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
 
@@ -100,7 +99,6 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -108,11 +106,10 @@ class App extends Component {
           onClick={this.togglePersonsHandler}>Switch Name
         </button>
         {persons}
-      </div>
-      </StyleRoot>
+      </div>      
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default Radium(App);
+export default App;
