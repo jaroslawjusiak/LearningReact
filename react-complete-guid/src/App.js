43665625c00f65584f538dfbd80a8cdaac9ece9e
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-10 08:30:10 +0200

    Revert "Lesson 81 - Error Boundries"
    This reverts commit 05405fb72b952de8333a674872ce5097da7fa487.

--- react-complete-guid/src/App.js
+++ react-complete-guid/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
 
@@ -54,16 +53,16 @@ class App extends Component {
     let btnClass = '';
 
     if(this.state.showPersons){
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-              return <ErrorBoundary key={person.id} >                
-                <Person
-                  click={() => this.deletePersonHandler(index)}
-                  name={person.name}
-                  age={person.age}                  
-                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
-              </ErrorBoundary>
+      persons =(
+        <div >
+          {
+            this.state.persons.map((person, index) => {
+              return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} 
+              key={person.id} 
+              changed={(event) => this.nameChangedHandler(event, person.id)}/>
             })
           }
         </div> 
