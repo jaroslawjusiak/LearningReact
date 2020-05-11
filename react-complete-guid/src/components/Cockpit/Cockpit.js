389b4ded6a34f8b08702a48f1c43b1080f2b27fc
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-11 19:24:12 +0200

    Lesson 95 - cleaning up

--- react-complete-guid/src/components/Cockpit/Cockpit.js
+++ react-complete-guid/src/components/Cockpit/Cockpit.js
@@ -6,9 +6,19 @@ const cockpit = (props) => {
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
     setTimeout(()=>{
-      alert('Saved data to cloud!');
+      console.log('Saved data to cloud!');
     },1000);
-  }, [])
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);
+
+  useEffect(() =>{
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] 2nd cleanup work in useEffect');
+    };
+  });
 
   //[props.persons] -> gdy zmieni sie zawartosc persons, to metoda ta odpali sie
   //[] -> dla puste tablicy uruchomi sie tylko 1 raz, gdy jest inicjowany komponent
