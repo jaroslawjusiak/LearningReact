efee882be2eef1aa12ab5af265eb89865502ad02
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-11 19:53:39 +0200

    Lesson 96 - cleaning up effect - remove timer

--- react-complete-guid/src/components/Cockpit/Cockpit.js
+++ react-complete-guid/src/components/Cockpit/Cockpit.js
@@ -5,10 +5,11 @@ const cockpit = (props) => {
     
   useEffect(() => {
     console.log('[Cockpit.js] useEffect');
-    setTimeout(()=>{
+    const timer = setTimeout(()=>{
       console.log('Saved data to cloud!');
     },1000);
     return () => {
+      clearTimeout(timer);
       console.log('[Cockpit.js] cleanup work in useEffect');
     };
   }, []);
