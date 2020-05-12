ea7bd2942cce318002d526b778cc5e92266b1896
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-12 21:07:28 +0200

    Lesson 102 - Aux

--- react-complete-guid/src/components/Persons/Person/Person.js
+++ react-complete-guid/src/components/Persons/Person/Person.js
@@ -1,18 +1,19 @@
 import React, {Component} from 'react';
 import classes from './Person.css';
+import Aux from '../../../hoc/Auxiliary';
 
 class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>        
+            </Aux>      
         )
     }
     
 }
-
+//Aux element is not a HTML element and will not be added as such into DOM
 export default Person;
