a35e1af6547ad39f2a648d3c0b29a55173bf7418
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-13 08:32:55 +0200

    Lesson 104 - React.Fragment - built in container

--- react-complete-guid/src/components/Persons/Person/Person.js
+++ react-complete-guid/src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import classes from './Person.css';
 import Aux from '../../../hoc/Auxiliary';
 
@@ -6,14 +6,26 @@ class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (
-            <Aux>
+            <Fragment>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Aux>      
+            </Fragment> 
+            // <Aux>
+            // <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
+            // <p>{this.props.children}</p>
+            // <input type="text" onChange={this.props.changed} value={this.props.name} />
+            //  </Aux>      
+            // <React.Fragment>
+            // <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
+            // <p>{this.props.children}</p>
+            // <input type="text" onChange={this.props.changed} value={this.props.name} />
+            // </React.Fragment>      
         )
     }
     
 }
 //Aux element is not a HTML element and will not be added as such into DOM
+//Built in Aux component is called React.Fragment
+//Instead of using default notation React.Fragment, we can import Fragment and use it instead
 export default Person;
