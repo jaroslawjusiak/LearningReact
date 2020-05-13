864ca1f22beafc7f70a88d0f63d09c52024bad01
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-13 12:53:11 +0200

    Lesson 110 - using refs

--- react-complete-guid/src/components/Persons/Person/Person.js
+++ react-complete-guid/src/components/Persons/Person/Person.js
@@ -5,13 +5,32 @@ import Aux from '../../../hoc/Auxiliary';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+    
+    constructor(props){
+        super(props);
+        this.inputElementRef = React.createRef();
+    }
+
+    componentDidMount(){
+        this.inputElementRef.current.focus();
+    }
+    
     render(){
         console.log('[Person.js] rendering...');
         return (
             <Aux>
-            <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
+            <p 
+                onClick={this.props.click}>
+                    I'm {this.props.name} and I'm {this.props.age} years old!
+            </p>
             <p>{this.props.children}</p>
-            <input type="text" onChange={this.props.changed} value={this.props.name} />
+            <input 
+                type="text" 
+                onChange={this.props.changed} 
+                value={this.props.name} 
+                //ref={(inputEl) => {this.inputElementRef = inputEl}}
+                ref={this.inputElementRef}
+                />
              </Aux>                    
         )
     }
