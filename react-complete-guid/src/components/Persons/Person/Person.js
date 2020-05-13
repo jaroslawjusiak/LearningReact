34043a30ebe296a9e6a42dc4695c5e8dd83289ac
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-13 08:43:16 +0200

    Lesson 105 - HOC Components introduction

--- react-complete-guid/src/components/Persons/Person/Person.js
+++ react-complete-guid/src/components/Persons/Person/Person.js
@@ -6,26 +6,14 @@ class Person extends Component {
     render(){
         console.log('[Person.js] rendering...');
         return (
-            <Fragment>
-                <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </Fragment> 
-            // <Aux>
-            // <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
-            // <p>{this.props.children}</p>
-            // <input type="text" onChange={this.props.changed} value={this.props.name} />
-            //  </Aux>      
-            // <React.Fragment>
-            // <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
-            // <p>{this.props.children}</p>
-            // <input type="text" onChange={this.props.changed} value={this.props.name} />
-            // </React.Fragment>      
+            <Aux>
+            <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
+            <p>{this.props.children}</p>
+            <input type="text" onChange={this.props.changed} value={this.props.name} />
+             </Aux>                    
         )
     }
     
 }
-//Aux element is not a HTML element and will not be added as such into DOM
-//Built in Aux component is called React.Fragment
-//Instead of using default notation React.Fragment, we can import Fragment and use it instead
+
 export default Person;
