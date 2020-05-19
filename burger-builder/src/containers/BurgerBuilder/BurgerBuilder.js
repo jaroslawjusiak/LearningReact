73a08baffe36d7c354e1f8356b4b5bdc88c755c7
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-19 18:49:47 +0200

    Lesson 135 - Display price

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,10 +14,10 @@ const INGREDIENT_PRICES = {
 class BurgerBuilder extends Component{
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         },
         totalPrice: 4    
     };
@@ -71,6 +71,7 @@ class BurgerBuilder extends Component{
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disableInfo}
+                    price={this.state.totalPrice}
                     />
             </Aux>
         );
