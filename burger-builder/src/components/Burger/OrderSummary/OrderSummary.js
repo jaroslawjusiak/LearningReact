1d00c4d1879b92df66dd5e76bdb72156f3af4d29
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-20 19:28:21 +0200

    Lesson 141 - Button Components in Modal

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from './../../../hoc/Auxiliary';
+import Button from './../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -17,8 +18,8 @@ const orderSummary = (props) => {
                 {ingredientSummary}
             </ul>
             <p>Continue to Checkout?</p>
-            <button>CANCEL</button>
-            <button>CONTINUE</button>
+            <Button clicked={props.purchaseCancelled} btnType="Danger">CANCEL</Button>
+            <Button clicked={props.purchaseContinued} btnType="Success">CONTINUE</Button>
         </Aux>
     );
 };
