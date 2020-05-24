69078284dba6c033741f3e43e2e7a149af74feea
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-05-24 11:22:32 +0200

    Lesson 149 - Hidding a sidedraw using backdrop

--- burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,20 +2,33 @@ import React from 'react';
 import classes from './SideDrawer.module.css';
 import Logo from './../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from './../../UI/Backdrop/Backdrop';
+import Aux from './../../../hoc/Auxiliary';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer];
+    
+    if (props.open){
+        attachedClasses.push(classes.Open)
+    }
+    else{
+        attachedClasses.push(classes.Close)
+    }
 
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
-            </div>
-            <nav>
-                <NavigationItems>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.closed}/>
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems>
 
-                </NavigationItems>
-            </nav>
-        </div>
+                    </NavigationItems>
+                </nav>
+            </div>
+        </Aux>
     );
 };
 
