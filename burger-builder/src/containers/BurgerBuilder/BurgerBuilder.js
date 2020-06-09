82cb5f138369dd8f1e47d103fb3e9fad5eff5291
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-09 20:10:31 +0200

    Lesson 224 - Navigating to checkout page

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -95,31 +95,32 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler = () => {
         //alert('You continue!');
-        this.setState({loading:true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Keraj Kaisuj',
-                address: {
-                    street: 'Niepodległości 8',
-                    postCode: '20-123',
-                    country: 'Poland'
-                },
-                email: 'keraj.kaisuj@gmail.com'
-            },
-            deliveryMethod: 'fastest'
-        };
+        // this.setState({loading:true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Keraj Kaisuj',
+        //         address: {
+        //             street: 'Niepodległości 8',
+        //             postCode: '20-123',
+        //             country: 'Poland'
+        //         },
+        //         email: 'keraj.kaisuj@gmail.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // };
         
-        axios.post('/orders.json', order)
-        .then(response => {
-            console.log(response);
-            this.setState({loading:false, purchasing:false});
-        })
-        .catch(error => {
-            console.log(error);
-            this.setState({loading:false, purchasing:false});
-        });
+        // axios.post('/orders.json', order)
+        // .then(response => {
+        //     console.log(response);
+        //     this.setState({loading:false, purchasing:false});
+        // })
+        // .catch(error => {
+        //     console.log(error);
+        //     this.setState({loading:false, purchasing:false});
+        // });
+        this.props.history.push('/checkout');
     }
 
     render(){
