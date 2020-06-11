d863450a2e34f24b9ca3c3d7bb2e7a58628f7a06
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-11 18:09:42 +0200

    Lesson 229 - Adding Orders page

---
+++ burger-builder/src/containers/Orders/Orders.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import classes from './Orders.module.css';
+import Oder from '../../components/Order/Order';
+
+class Orders extends Component{
+    state = {
+    
+    };
+
+    componentDidMount(){
+
+    };
+
+    componentDidUpdate(){
+
+    };
+
+    render(){
+        return (
+            <div>
+                <Orders />
+                <Orders />
+            </div>
+        );
+    };
+
+};
+
+export default Orders;
