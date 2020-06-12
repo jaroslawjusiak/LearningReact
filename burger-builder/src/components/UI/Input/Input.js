24fa9aea5eb378969732b76164a22124b09b2ea4
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-12 18:05:53 +0200

    Lesson 220 - Adding a Dropdown element

--- burger-builder/src/components/UI/Input/Input.js
+++ burger-builder/src/components/UI/Input/Input.js
@@ -17,6 +17,19 @@ const input = (props) => {
                 value={props.value} 
                 className={classes.InputElement}/>;
             break;
+        case('select'):
+            inputElement = (
+            <select 
+                value={props.value} 
+                className={classes.InputElement}>
+                {props.elementConfig.options.map(option =>(
+                    <option key={option.value} value={option.value}>
+                        {option.displayValue}
+                    </option>
+                ))}
+            </select>
+            );
+            break;
         default:
             inputElement = <input 
                 {...props.elementConfig} 
