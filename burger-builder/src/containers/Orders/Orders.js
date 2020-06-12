e0b9d7e5b7d62dab8fbf63759147de51071affb6
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-12 10:46:31 +0200

    Lesson 231 - Fetching orders from firebase

--- burger-builder/src/containers/Orders/Orders.js
+++ burger-builder/src/containers/Orders/Orders.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react';
 import classes from './Orders.module.css';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component{
     state = {
-    
+        orders: [],
+        loading: true
     };
 
     componentDidMount(){
-
+        axios.get('/orders.json')
+        .then(res=> {
+            const fetchedOrders = [];
+            for(let key in res.data){
+                fetchedOrders.push({
+                    ...res.data[key],
+                    id: key
+                });
+            }
+            this.setState({orders: fetchedOrders});
+        })
+        .catch(err => {
+            console.log('[Orders] componentDidMount -> get /orders error');
+            console.log(err);
+        })
+        .then(()=>{
+            this.setState({loading:false});
+        });
     };
 
     componentDidUpdate(){
@@ -26,4 +46,4 @@ class Orders extends Component{
 
 };
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
