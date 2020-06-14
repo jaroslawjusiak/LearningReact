d6f30fe1df8aac85f83aaf217ce242a3ed7e9f70
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-14 16:27:35 +0200

    Lesson 257 - Dispatching actions

--- redux--01-start/redux-basics.js
+++ redux--01-start/redux-basics.js
@@ -8,6 +8,20 @@ const initialState = {
 // Reducer
 // state = initialState takes default value instead of previous state, in that case it takes a value of initialState object;
 const rootReducer = (state = initialState, action) => {
+    if(action.type === 'INC_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+
+    if(action.type === 'ADD_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+
     return state;
 };
 
@@ -15,6 +29,9 @@ const rootReducer = (state = initialState, action) => {
 // Store
 const store = createStore(rootReducer);
 console.log(store.getState());
-// Dispatching Action
 
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 // Subscription
