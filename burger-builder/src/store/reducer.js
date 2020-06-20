c91528e401e5062b63a81c0854a8cbea0defc475
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-20 09:27:34 +0200

    Lesson 277 - Finishing the reducer for ingredients

--- burger-builder/src/store/reducer.js
+++ burger-builder/src/store/reducer.js
@@ -1,16 +1,33 @@
-import * as actionTypes from '../store/actions';
+import * as actionTypes from "../store/actions";
 
 const initialState = {
-    ingredients: null,
-    price: 4
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0,
+    },
+    price: 4,
 };
 
 const reducer = (state = initialState, action) => {
-    switch(action){
-        case(actionTypes.ADD_INGREDIENT):
-            return state;
-        case(actionTypes.REMOVE_INGREDIENT):
-            return state;
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+                },
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+                },
+            };
         default:
             return state;
     }
