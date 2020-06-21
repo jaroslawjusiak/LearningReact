7aaf8daf9032893c3bbcfe4cc2298e90154a6f9b
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-21 17:05:35 +0200

    Lesson 304 - Executing asynchronous code

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { BrowserRouter } from 'react-router-dom';
+import thunk from 'redux-thunk';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 
-const store = createStore(
-	burgerBuilderReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(burgerBuilderReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<React.StrictMode>
