c00df58b9af71523f26d9793b617f65e1735b9fa
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-21 19:49:22 +0200

    Lesson 308 - Adding Order Actions

--- burger-builder/src/store/actions/order.js
+++ burger-builder/src/store/actions/order.js
@@ -1,3 +1,31 @@
 import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
 
-//export const
+export const purchaseBurgerSuccess = (id, orderData) => {
+	return {
+		type: actionTypes.PURCHASE_BURGER_SUCCESS,
+		orderId: id,
+		orderData: orderData
+	};
+};
+
+export const purchaseBurgerFail = error => {
+	return {
+		type: actionTypes.PURCHASE_BURGER_FAIL,
+		error: error
+	};
+};
+
+export const purchaseBurgerAsync = orderData => {
+	return dispatch => {
+		axios.post('/orders.json', orderData)
+			.then(response => {
+				console.log(response.data);
+				dispatch(purchaseBurgerSuccess(response.data, orderData));
+			})
+			.catch(error => {
+				console.log(error);
+				dispatch(purchaseBurgerFail(error));
+			});
+	};
+};
