57e2c318a79a1acf6e6b2ca76bbc61c498935ebe
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-21 11:30:31 +0200

    Lesson 291 - Restructuring actions

---
+++ redux--01-start/src/store/actions/index.js
@@ -0,0 +1,2 @@
+export { add, subtract, increment, decrement } from './counter';
+export { storeResultAsync, deleteResult } from './result';
---
+++ redux--01-start/src/store/actions/result.js
@@ -0,0 +1,23 @@
+import * as actionTypes from './actionTypes';
+
+export const storeResult = result => {
+	return {
+		type: actionTypes.STORE_RESULT,
+		result: result
+	};
+};
+
+export const storeResultAsync = result => {
+	return dispatch => {
+		setTimeout(() => {
+			dispatch(storeResult(result));
+		}, 2000);
+	};
+};
+
+export const deleteResult = id => {
+	return {
+		type: actionTypes.DELETE_RESULT,
+		id: id
+	};
+};
