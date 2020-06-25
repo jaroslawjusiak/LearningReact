123ced03dba8037a311b1b80cca5a68a08955b33
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-25 18:10:47 +0200

    Lesson 327 - Getting the token from the backend

--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -1,5 +1,7 @@
 import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
+import axios from 'axios';
+
+const apiKey = 'AIzaSyC6QA9FhhC2VpfKFN1HO4ESKbVVj28_a7M';
 
 export const authStart = () => {
     return {
@@ -24,5 +26,22 @@ export const AuthFail = (error) => {
 export const authAsync = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`, authData)
+            .then(res => {
+                console.log('[authAsync]: signing UP success', res.data);
+                dispatch(AuthSuccess(res.data));
+            })
+            .catch(err => {
+                console.log('[authAsync]: signing UP error', err);
+                dispatch(AuthFail(err));
+            });
     };
 };
+
+
+//axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${apiKey}`, authData)
