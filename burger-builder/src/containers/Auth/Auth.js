6d35ec0ba16bf650cefb311a75ad4f5811966a75
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-06-30 19:04:39 +0200

    Lesson 347 - using updateObject in the entire app

--- burger-builder/src/containers/Auth/Auth.js
+++ burger-builder/src/containers/Auth/Auth.js
@@ -6,6 +6,7 @@ import { connect } from 'react-redux';
 import * as actions from '../../store/actions/index';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import { Redirect } from 'react-router-dom';
+import { updateObject } from '../../shared/utility';
 
 class Auth extends Component {
     state = {
@@ -90,17 +91,20 @@ class Auth extends Component {
     };
 
     inputChangedHandler = (event, controlName) => {
-        const updatedControls = {
-            ...this.state.controls,
-            [controlName]: {
-                ...this.state.controls[controlName],
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
                 valid: this.checkValidity(
                     event.target.value,
                     this.state.controls[controlName].validation
                 ),
                 touched: true
-            }
+            })
+        });
+
+        const updatedControls = {
+            ...this.state.controls,
+
         };
         this.setState({ controls: updatedControls });
     };
--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,7 @@ import Input from '../../../components/UI/Input/Input';
 import { connect } from 'react-redux';
 import withErrorHandler from '../../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../../store/actions/order';
+import { updateObject } from '../../../shared/utility';
 
 class ContactData extends Component {
 	state = {
@@ -135,18 +136,15 @@ class ContactData extends Component {
 	};
 
 	inputChangedHandler = (event, inputId) => {
-		const updatedOrderForm = {
-			...this.state.orderForm
-		};
+		const updatedFormElement = updateObject(this.state.orderForm[inputId], {
+			value: event.target.value,
+			valid: this.checkValidity(event.target.value, this.state.orderForm[inputId].validation),
+			touched: true
+		});
 
-		const updatedFormElement = {
-			...updatedOrderForm[inputId]
-		};
-
-		updatedFormElement.value = event.target.value;
-		updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
-		updatedFormElement.touched = true;
-		updatedOrderForm[inputId] = updatedFormElement;
+		const updatedOrderForm = updateObject(this.state.orderForm, {
+			[inputId]: updatedFormElement
+		});
 
 		let formIsValid = true;
 		for (let inputId in updatedOrderForm) {
