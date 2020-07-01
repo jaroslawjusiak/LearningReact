390c710ef73f8839301d01ab87dc3ddbf4136b48
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-07-01 19:43:16 +0200

    Lesson 350 - Adding lazy loading, env var fix

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -3,11 +3,22 @@ import { connect } from 'react-redux';
 import * as actions from './store/actions/index';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
 import { Route, Switch, Redirect } from 'react-router-dom';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+
 import Logout from './containers/Auth/Logout/Logout';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
 
 function App(props) {
   //Checking local storage for valid session
@@ -15,7 +26,7 @@ function App(props) {
 
   let routes = (
     <Switch>
-      <Route path="/auth" component={Auth} />
+      <Route path="/auth" component={asyncAuth} />
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to="/" />
     </Switch>
@@ -24,10 +35,10 @@ function App(props) {
   if (props.isAuthenticated) {
     routes = (
       <Switch>
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/orders" component={Orders} />
+        <Route path="/checkout" component={asyncCheckout} />
+        <Route path="/orders" component={asyncOrders} />
         <Route path="/logout" component={Logout} />
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={asyncAuth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
