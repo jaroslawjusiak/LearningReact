5e3b52ae2fba8d67679abffd02d80be9dd33e5a7
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-07-02 18:59:14 +0200

    Lesson 359 - wrapper.contains.toEqual(true)

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -30,4 +30,9 @@ describe('<NavigationItems />', () => {
         wrapper.setProps({ isAuthenticated: true });
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+
+    it('should render <NavigationItems /> containing Logout NavigationItem', () => {
+        wrapper.setProps({ isAuthenticated: true });
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+    });
 });
