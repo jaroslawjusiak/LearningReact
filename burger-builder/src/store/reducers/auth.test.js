797a7e4274e4ed4087acfad5e15867ce55e8ff98
jaroslaw.jusiak <jaroslaw.jusiak@beqom.com>
2020-07-02 19:43:55 +0200

    Lesson 362 - How to test Redux

---
+++ burger-builder/src/store/reducers/auth.test.js
@@ -0,0 +1,35 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    let initialState;
+
+    beforeEach(() => {
+        initialState = {
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            redirectPath: '/'
+        };
+    });
+
+
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual(initialState);
+    });
+
+    it('should store token upon login', () => {
+        expect(reducer(initialState,
+            {
+                type: actionTypes.AUTH_SUCCESS,
+                idToken: 'some-token',
+                userId: 'some-user-id'
+            }
+        )).toEqual({
+            ...initialState,
+            token: 'some-token',
+            userId: 'some-user-id'
+        });
+    });
+});
